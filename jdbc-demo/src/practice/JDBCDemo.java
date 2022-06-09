29a0d82b9fb83c18d5d3e31380f2cca49c8aec25
shinyawhy <505064340@qq.com>
2022-06-09 22:31:27 +0800

    jdbc联系

---
+++ jdbc-demo/src/practice/JDBCDemo.java
@@ -0,0 +1,33 @@
+package practice;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * JDBC快速入门
+ */
+public class JDBCDemo {
+
+
+    public static void main(String[] args) throws Exception{
+        // 注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        // 获取链接
+        String url = "jdbc:mysql://127.0.0.1:3306/test";
+        String username = "root";
+        String password = "root";
+        Connection conn = DriverManager.getConnection(url,username,password);
+
+        String sql = "UPDATE db1 SET money = 2000 WHERE id = 1";
+
+        Statement stmt = conn.createStatement();
+
+        int count = stmt.executeUpdate(sql);
+
+        System.out.println(count);
+
+        stmt.close();
+        conn.close();
+    }
+}
