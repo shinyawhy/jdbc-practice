c794eda0b39b51c6b0e3bd47cbdaf7366db65ca2
shinyawhy <505064340@qq.com>
2022-06-09 22:37:54 +0800

    jdbc练习

--- jdbc-demo/src/practice/JDBCDemo.java
+++ jdbc-demo/src/practice/JDBCDemo.java
@@ -18,11 +18,11 @@ public class JDBCDemo {
         String username = "root";
         String password = "root";
         Connection conn = DriverManager.getConnection(url,username,password);
-
+        // 定义sql
         String sql = "UPDATE db1 SET money = 2000 WHERE id = 1";
-
+        // 获取执行sql对象
         Statement stmt = conn.createStatement();
-
+        // 执行sql
         int count = stmt.executeUpdate(sql);
 
         System.out.println(count);
